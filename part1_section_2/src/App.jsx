6b35f0d7efba7c35a8e7a863c67104912ae314e8
Carl <carlctha1@gmail.com>
2023-11-30 18:10:11 +0100

    Exercise 1.9

--- part1_section_2/src/App.jsx
+++ part1_section_2/src/App.jsx
@@ -29,7 +29,6 @@ const Statistics = ({ all, average, positive }) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -48,10 +47,17 @@ const App = () => {
       <Button handleClick={() => changeValue(neutral, setNeutral, 1)} text="neutral"/>
       <Button handleClick={() => changeValue(bad, setBad, 1)} text="bad"/>
       <Header heading="statistics" />
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
-      <Statistics all={all} average={average} positive={positive} />
+      
+      {good > 0 || neutral > 0 || bad > 0 ? (
+        <div>
+          <Display value={good} text="good" />
+          <Display value={neutral} text="neutral" />
+          <Display value={bad} text="bad" />
+          <Statistics all={all} average={average} positive={positive} />
+        </div>
+      ) : (
+        <div>No feedback given</div>
+      )}
     </div>
   )
 }
