24212e44ba016d1efb114977a21e0c51b6597703
Carl <carlctha1@gmail.com>
2023-11-30 17:29:49 +0100

    Exercise 1.6

--- part1_section_2/src/App.jsx
+++ part1_section_2/src/App.jsx
@@ -1,14 +1,43 @@
 import { useState } from 'react'
 
+const Header = ({ heading }) => {
+  return (
+    <h1>{heading}</h1>
+  )
+}
+
+const Display = ( {text, value} ) => {
+  return (
+    <div>{text} {value}</div>
+  )
+}
+
+const Button = ( {handleClick, text} ) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const changeValue = (value, setValue, newValue) => {
+    setValue(value + newValue)
+  }
+
   return (
     <div>
-      code here
+      <Header heading="give feedback"/>
+      <Button handleClick={() => changeValue(good, setGood, 1)} text="good"/>
+      <Button handleClick={() => changeValue(neutral, setNeutral, 1)} text="neutral"/>
+      <Button handleClick={() => changeValue(bad, setBad, 1)} text="bad"/>
+      <Header heading="statistics" />
+      <Display value={good} text="good" />
+      <Display value={neutral} text="neutral" />
+      <Display value={bad} text="bad" />
     </div>
   )
 }
