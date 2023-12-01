5a16c89671d02ee04a88ab0a8d7a91da0bd73d6e
Carl <carlctha1@gmail.com>
2023-12-01 23:01:09 +0100

    Exercise 2.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -45,32 +45,52 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      },
-      {
-        name: "Redux",
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7
+        },
+        {
+          name: "State of a component",
+          exercises: 14
+        },
+        {
+          name: "Redux",
+          exercises: 11
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const courses_array = courses.map(part => (
+    <Course course={part}/>
+  ))
 
   return (
-    <div>
-      <Course course={course}/>
-    </div>
+    courses_array
   )
 }
 
