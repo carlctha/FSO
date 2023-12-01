f30564b341888a396bc127024a36ad5f0772eb23
Carl <carlctha1@gmail.com>
2023-12-02 00:58:12 +0100

    Exercise 2.8

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 
 const Person = ( {person} ) => {
   return (
-    <li>{person.name}</li>
+    <li>{person.name} {person.number}</li>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      name: 'Arto Hellas',
+      name: "Arto Hellas",
+      number: "040-123456",
       id: 1
     }
   ]) 
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
-  const addName = (event) => {
+  const addInput = (event) => {
     event.preventDefault()
 
     const nameInPersons = persons.find(
@@ -28,6 +30,7 @@ const App = () => {
     else {
       const personObj = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1
       }
 
@@ -37,17 +40,25 @@ const App = () => {
 
   }
 
-  const handleInput = (event) => {
+  const handleInputName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleInputNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addInput}>
         <div>
-          name: <input value={newName} onChange={handleInput}/>
+          name: <input value={newName} onChange={handleInputName}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleInputNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
