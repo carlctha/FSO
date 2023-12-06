57ffb4c49ad5025c83580e361fc3b67e7a500b7c
Carl <carlctha1@gmail.com>
2023-12-06 20:51:35 +0100

    Exercise 2.20

--- part2_section_2/src/App.jsx
+++ part2_section_2/src/App.jsx
@@ -1,6 +1,47 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const Weather = ({ latLong }) => {
+  if (latLong === null) {
+    return <div></div>
+  }
+
+  const [data, setData] = useState(null)
+  const [pngId, setPngId] = useState(null)
+  const latitude = latLong[0]
+  const longitude = latLong[1]
+  const apiKey = import.meta.env.VITE_SOME_KEY
+
+  useEffect(() => {
+    axios
+      .get(
+        `
+        https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}
+        `
+      )
+      .then(response => {
+        console.log(response.data)
+        setData(response.data)
+        setPngId(response.data.weather[0].icon)
+      }).catch(error => {
+        console.error("Error retrieving data from weather api", error)
+      })
+  }, [latitude, longitude])
+
+  return (
+    <div>
+      {data && (
+      <div>
+        <h2>Weather in {data.name}</h2>
+        <div>Temperature {data.main.temp -273.15} Celsius</div>
+        <img src={`https://openweathermap.org/img/wn/${pngId}@2x.png`} alt="YEEEET" />
+        <div>Wind {data.wind.speed} m/s</div>
+      </div>
+      )}
+    </div>
+  )
+}
+
 const Part = ({ languages }) => {
   return (
     <ul>
@@ -27,12 +68,12 @@ const Display = ({ country }) => {
 const CountryList = ({ countries, handleClick }) => {
   return (
     <div>
-      {countries.map(country => (
-        <div key={country.name.official}>
-          {country.name.common}
-        <button onClick={() => handleClick(country)}>show</button>
-        </div>
-      ))}
+    {countries.map(country => (
+      <div key={country.name.official}>
+        {country.name.common}
+      <button onClick={() => handleClick(country)}>show</button>
+      </div>
+    ))}
     </div>
   )
 }
@@ -40,12 +81,12 @@ const CountryList = ({ countries, handleClick }) => {
 const App = () => {
   const [data, setData] = useState(null)
   const [display, setDisplay] = useState([])
+  const [latLong, setLatLong] = useState(null)
 
   useEffect(() => {
     axios
     .get("https://studies.cs.helsinki.fi/restcountries/api/all")
     .then(response => {
-      console.log(response.data[169])
       setData(response.data)
     })
   }, [])
@@ -66,15 +107,18 @@ const App = () => {
 
     if (countries.length >= 10) {
       setDisplay("To many matches, specify another filter")
+      setLatLong(null)
     }
     else if (countries.length < 10 && countries.length > 1) {
       const countryList = <CountryList
         countries={countries} handleClick={handleClick} 
       />
       setDisplay(countryList)
+      setLatLong(null)
     }
     else {
       const info = <Display country={countries[0]}/>
+      setLatLong(countries[0].latlng)
       setDisplay(info)
     }
   }
@@ -83,6 +127,7 @@ const App = () => {
     <div>
       find countries<input type="text" onChange={handleSearch}/>
       <div>{display}</div>
+      <Weather latLong={latLong}/>
     </div>
   )
 }
